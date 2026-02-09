507cf88c607b4489c8e0a0f7f9f9332703f96c5a
BlowUpMx <blowupexperiencemexico@gmail.com>
2026-02-08 22:56:45 -0600

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,11 @@
-async function api(data){
- const r = await fetch(API_URL,{method:'POST',body:JSON.stringify(data)});
- return r.json();
+async function api(data) {
+  const res = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+
+  return await res.json();
 }
